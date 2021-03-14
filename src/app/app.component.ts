144e1733e1680b5c683995037579cd07c5895aab
Tobias Emrich <tobiemrich@gmail.com>
2021-03-14 11:17:49 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'list-test';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  
+
+  header = {
+    id: 'Personalnummer',
+    firstName: 'Vorname',
+    lastName: 'Nachname',
+    code: 'Einladungscode',
+  };
+
+  people = Array(20)
+    .fill(1)
+    .map((_) => {
+      return {
+        id: 123456,
+        firstName: 'Max',
+        lastName: 'Mustermann',
+        code: '123456789012',
+      };
+    });
 }
